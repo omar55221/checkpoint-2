ae7aff94e07bc9cde8c69b21ff09b674ce88c77d
omar55221 <omar91010@gmail.com>
2023-07-27 10:51:09 +0000

    Step 2: Composition - Added HydraulicSystemMonitor to AGVRobotInfo

--- include/robot_info/agv_robot_info_class.h
+++ include/robot_info/agv_robot_info_class.h
@@ -4,14 +4,16 @@
 
 #include "robot_info/robot_info.h"
 #include "robotinfo_msgs/RobotInfo10Fields.h"
+#include "robot_info/hydraulic_system_monitor.h"
 
 class AGVRobotInfo : public RobotInfo
 {
     public:
         AGVRobotInfo();
-        void publish_data() override;
-    private:
-        std::string maximum_payload;
+        virtual void publish_data();
+private:
+  std::string maximum_payload;
+  HydraulicSystemMonitor hydraulic_system;
 };
 
 #endif // AGV_ROBOT_INFO_H
--- src/agv_robot_info_class.cpp
+++ src/agv_robot_info_class.cpp
@@ -2,6 +2,9 @@
 
 AGVRobotInfo::AGVRobotInfo() : RobotInfo() {
   maximum_payload = "100 Kg";
+  hydraulic_system.hydraulic_oil_temperature = "45C";
+  hydraulic_system.hydraulic_oil_tank_fill_level = "100%";
+  hydraulic_system.hydraulic_oil_pressure = "250 bar";
 }
 
 void AGVRobotInfo::publish_data() {
@@ -11,9 +14,12 @@ void AGVRobotInfo::publish_data() {
   info.data_field_03 = "ip_address: 169.254.5.180";
   info.data_field_04 = "firmware_version: 3.5.8";
   info.data_field_05 = "maximum_payload: " + maximum_payload;
+  info.data_field_06 = "hydraulic_oil_temperature: " + hydraulic_system.getTemperature();
+  info.data_field_07 = "hydraulic_oil_tank_fill_level: " + hydraulic_system.getFillLevel();
+  info.data_field_08 = "hydraulic_oil_pressure: " + hydraulic_system.getPressure();
 
   while (ros::ok()) {
-    robotInfo_Publisher.publish(info);  // Publish 'info' instead of 'info_msg'
+    robotInfo_Publisher.publish(info);
     ros::spinOnce();
     loop_rate.sleep();
   }
